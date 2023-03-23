eabf1cd97a476a264c9f52903b6c1f4456c3c7e4
revazashvili <levanrevazashvili@gmail.com>
2023-03-23 20:13:22 +0400

    use models for parameter and return type

--- src/Infrastructure/Services/TokenGenerator.cs
+++ src/Infrastructure/Services/TokenGenerator.cs
@@ -11,15 +11,22 @@ namespace Infrastructure.Services;
 /// <inheritdoc cref="ITokenGenerator"/>
 public class TokenGenerator : ITokenGenerator
 {
-    public string Generate(string secretKey, string issuer, string audience, double expires, IEnumerable<Claim>? claims = null)
+    /// <summary>
+    /// Generates a security token based on the provided request.
+    /// </summary>
+    /// <param name="generateTokenRequest">The request containing information about the token to be generated.</param>
+    /// <returns>The generated security token response.</returns>
+    public GenerateTokenResponse Generate(GenerateTokenRequest generateTokenRequest)
     {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(generateTokenRequest.SecretKey));
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        JwtSecurityToken securityToken = new(issuer,audience,
-            claims,
+        JwtSecurityToken securityToken = new(generateTokenRequest.Issuer,
+            generateTokenRequest.Audience,
+            generateTokenRequest.Claims,
             DateTime.UtcNow,
-            DateTime.UtcNow.AddMinutes(expires),
+            DateTime.UtcNow.AddMinutes(generateTokenRequest.Expires),
             credentials);
-        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+        return new GenerateTokenResponse(token!);
     }
 }
