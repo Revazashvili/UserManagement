f72b043e08c3ac8fb045cee4511b4f2c3945f2f0
revazashvili <levanrevazashvili@gmail.com>
2023-03-26 16:59:56 +0400

    add infrastructure arch tests

---
+++ tests/Infrastructure.ArchTests/ServicesTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace Infrastructure.ArchTests;
+
+public class ServicesTests
+{
+    private static readonly PredicateList ServicesPredicateList = Types
+        .InAssembly(typeof(ServiceCollectionExtension).Assembly)
+        .That().ResideInNamespace("Infrastructure.Services")
+        .And()
+        .AreClasses();
+    
+    [Fact]
+    public void Services_ShouldBe_Sealed()
+    {
+        var testResult = ServicesPredicateList
+            .Should()
+            .BeSealed()
+            .GetResult();
+        
+        if(testResult.FailingTypeNames is not null && testResult.FailingTypeNames.Any())
+            testResult.FailingTypeNames.ToList().ForEach(typeName => Debug.WriteLine(typeName));
+        
+        Assert.True(testResult.IsSuccessful);
+    }
+    
+    [Fact]
+    public void Services_ShouldNotBe_Public()
+    {
+        var testResult = ServicesPredicateList
+            .ShouldNot()
+            .BePublic()
+            .GetResult();
+        
+        if(testResult.FailingTypeNames is not null && testResult.FailingTypeNames.Any())
+            testResult.FailingTypeNames.ToList().ForEach(typeName => Debug.WriteLine(typeName));
+        
+        Assert.True(testResult.IsSuccessful);
+    }
+}
